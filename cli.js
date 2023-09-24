01a30b02083fdf2de6b24ad76a8822971a9e0682
Ekansh Jain <ejekanshjain@gmail.com>
2023-09-24 11:50:43 +0530

    create advanced template

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const { prompt } = require('enquirer')
 const copydir = require('copy-dir')
 
 program
-  .version('1.0.0')
+  .version('2.0.0')
   .description('CLI tool to bootstrap a Next.js App')
   .action(async () => {
     const response = await prompt([
@@ -22,10 +22,23 @@ program
         name: 'description',
         message: 'Enter a description for the project:',
         initial: 'A Legendary Next.js App'
+      },
+      {
+        type: 'select',
+        name: 'template',
+        message: 'Select a template:',
+        choices: [
+          { name: 'Base', value: 'base' },
+          {
+            name: 'With user types and ACLs (roles & permissions)',
+            value: 'advanced'
+          }
+        ],
+        initial: 0
       }
     ])
 
-    const { projectName, description } = response
+    const { projectName, description, template } = response
 
     // Create a directory with the project name
     const projectDir = path.join(process.cwd(), projectName)
@@ -38,7 +51,7 @@ program
     fs.mkdirSync(projectDir)
 
     // Copy all files and subdirectories from your template folder to the new project directory
-    const templateDir = path.join(__dirname, 'template')
+    const templateDir = path.join(__dirname, 'template', template)
     copydir.sync(templateDir, projectDir)
 
     // Modify package.json in the new project directory to set the project name and description
@@ -54,6 +67,11 @@ program
     siteConfig = siteConfig.replace('Project Name', projectName)
     siteConfig = siteConfig.replace('Project Description', description)
     fs.writeFileSync(siteConfigFile, siteConfig)
+
+    // Copy .env.example to .env
+    const envExampleFile = path.join(projectDir, '.env.example')
+    const envExample = fs.readFileSync(envExampleFile, 'utf-8')
+    fs.writeFileSync(path.join(projectDir, '.env'), envExample)
 
     console.log(`Created a new Next.js app called '${projectName}'
 
