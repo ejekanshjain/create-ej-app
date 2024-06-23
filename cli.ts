f9060ab53b9b520bf7eba0b393178af57c622395
Ekansh Jain <ejekanshjain@gmail.com>
2024-06-23 10:24:02 +0530

    rename

--- cli.ts
+++ cli.ts
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import { prompt } from 'enquirer';
-import fs from 'fs';
-import path from 'path';
+import { program } from 'commander'
+import { prompt } from 'enquirer'
+import fs from 'fs'
+import path from 'path'
 //@ts-ignore
-import copydir from 'copy-dir';
+import copydir from 'copy-dir'
 
 interface Response {
-  projectName: string;
-  description: string;
-  template: string;
+  projectName: string
+  description: string
+  template: string
 }
 
 program.action(async () => {
@@ -34,48 +34,55 @@ program.action(async () => {
       choices: [
         {
           name: 'base',
+          message:
+            'Base setup with ts, shadcn, auth, drizzle, user types, ACLs (roles & permissions)'
+        },
+        {
+          name: 'prisma',
           message: 'Base setup with ts, shadcn, prisma, auth, editor.js'
         },
         {
-          name: 'advanced',
+          name: 'prisma-advanced',
           message:
             'Advanced setup with User Types and ACLs (roles & permissions)'
         }
       ]
     }
-  ]);
+  ])
 
-  const { projectName, description, template } = response;
+  const { projectName, description, template } = response
 
-  const projectDir = path.join(process.cwd(), projectName);
+  const projectDir = path.join(process.cwd(), projectName)
 
   if (fs.existsSync(projectDir)) {
-    console.error(`Error: Directory '${projectName}' already exists.`);
-    process.exit(1);
+    console.error(`Error: Directory '${projectName}' already exists.`)
+    process.exit(1)
   }
 
-  fs.mkdirSync(projectDir);
+  fs.mkdirSync(projectDir)
 
-  const templateDir = path.join(__dirname, 'template', template);
-  copydir.sync(templateDir, projectDir);
+  const templateDir = path.join(__dirname, 'template', template)
+  copydir.sync(templateDir, projectDir)
 
-  const packageJsonFile = path.join(projectDir, 'package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf-8'));
-  packageJson.name = projectName;
-  packageJson.description = description;
-  fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
+  const packageJsonFile = path.join(projectDir, 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf-8'))
+  packageJson.name = projectName
+  packageJson.description = description
+  fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2))
 
-  const siteConfigFile = path.join(projectDir, 'src', 'lib', 'siteConfig.ts');
-  let siteConfig = fs.readFileSync(siteConfigFile, 'utf-8');
-  siteConfig = siteConfig.replace('Project Name', projectName);
-  siteConfig = siteConfig.replace('Project Description', description);
-  fs.writeFileSync(siteConfigFile, siteConfig);
+  const siteConfigFile = path.join(projectDir, 'src', 'lib', 'siteConfig.ts')
+  let siteConfig = fs.readFileSync(siteConfigFile, 'utf-8')
+  siteConfig = siteConfig.replace('Project Name', projectName)
+  siteConfig = siteConfig.replace('Project Description', description)
+  fs.writeFileSync(siteConfigFile, siteConfig)
 
-  const envExampleFile = path.join(projectDir, '.env.example');
-  const envExample = fs.readFileSync(envExampleFile, 'utf-8');
-  fs.writeFileSync(path.join(projectDir, '.env'), envExample);
+  const envExampleFile = path.join(projectDir, '.env.example')
+  const envExample = fs.readFileSync(envExampleFile, 'utf-8')
+  fs.writeFileSync(path.join(projectDir, '.env'), envExample)
 
-  console.log(`Created a new app called '${projectName}' 🎉\n\nTo get started, run the following commands:\n\ncd ${projectName}\nnpm install\nnpm run dev\n`);
-});
+  console.log(
+    `Created a new app called '${projectName}' 🎉\n\nTo get started, run the following commands:\n\ncd ${projectName}\nnpm install\nnpm run dev\n`
+  )
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
