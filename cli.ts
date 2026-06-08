46c94eb6bcdd1d5a5737aa323614dde763a65063
Ekansh Jain <ejekanshjain@gmail.com>
2026-06-08 11:04:13 +0530

    fix

--- cli.ts
+++ cli.ts
@@ -5,6 +5,9 @@ import { program } from 'commander'
 import enquirer from 'enquirer'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 interface Response {
   projectName: string
